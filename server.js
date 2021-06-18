d7c974a071a08e890e16c5585e71c4528ab6e1f7
Keshav <keshavtayal002@gmail.com>
2021-06-18 18:53:52 +0530

    created cart and login pages

--- server.js
+++ server.js
@@ -8,14 +8,28 @@ const { E2BIG } = require('constants');
 
 //assets
 app.use(express.static('public')); //used to tell where is our assets eg. css
-app.get('/', (req, res) => {
-    res.render('home');
-})
 
 // set template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, 'resources/views'));
 app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {  //routes should be below owr setup
+    res.render('home');
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register');
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
